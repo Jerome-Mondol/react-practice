67c0008e5ec455999aab9513b47cceedf288d87f
Jerome Mondol <jeromemondol11@gmail.com>
2025-05-08 22:33:02 +0600

    init project

---
+++ react-mini-project/src/components/Form.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <>
+        <div className="flex flex-col items-center justify-center h-screen bg-slate-300">\
+            <div className="w-full max-w-sd bg-white rounded-lg">
+                
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Form
